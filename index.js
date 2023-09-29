eba70599685db8e628e054ad083657fffe270b97
MrNiNaD <ninadparkar273@gmail.com>
2023-09-30 02:05:30 +0530

    feat: reference code added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { hashPassword, comparePassword } = require('./util/login.util')
 const { UserModel } = require('./model/users')
+const jwt = require('jsonwebtoken');
 require('./db')
 const app = express()
 const port = 8080
@@ -13,10 +14,16 @@ const test = async () => {
   const afterHasing = await hashPassword('12345');
 
   const testHash = '$2b$10$QyBWA3kPnBasNn9LnZoIFOHLu6Nngr1.8B1LbBTmLjdVk.2qbSb7e';
+
+  const testJwt = jwt.sign({
+    data: 'foobar2222'
+  }, 'secret', { expiresIn: 10 });
+
+  const testVerify = jwt.verify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoiZm9vYmFyMjIyMiIsImlhdCI6MTY5NjAxODU5NywiZXhwIjoxNjk2MDE4NjU3fQ.H4hYBQUdOq763_CkWZB6AuhmYfOVFfGU84s9bq11u3M', 'secret')
   
   const compareTest = await comparePassword('12345', testHash);
 
-  console.log({ afterHasing, compareTest });
+  console.log({ afterHasing, compareTest, testJwt, testVerify });
 }
 
 
