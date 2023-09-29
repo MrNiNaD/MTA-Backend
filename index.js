dd4ca587fdbeae0bb7d60de95c3c331abb5148ea
MrNiNaD <ninadparkar273@gmail.com>
2023-09-30 02:06:12 +0530

    refactor: minor changes

--- index.js
+++ index.js
@@ -10,25 +10,6 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }))
 
-const test = async () => {
-  const afterHasing = await hashPassword('12345');
-
-  const testHash = '$2b$10$QyBWA3kPnBasNn9LnZoIFOHLu6Nngr1.8B1LbBTmLjdVk.2qbSb7e';
-
-  const testJwt = jwt.sign({
-    data: 'foobar2222'
-  }, 'secret', { expiresIn: 10 });
-
-  const testVerify = jwt.verify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoiZm9vYmFyMjIyMiIsImlhdCI6MTY5NjAxODU5NywiZXhwIjoxNjk2MDE4NjU3fQ.H4hYBQUdOq763_CkWZB6AuhmYfOVFfGU84s9bq11u3M', 'secret')
-  
-  const compareTest = await comparePassword('12345', testHash);
-
-  console.log({ afterHasing, compareTest, testJwt, testVerify });
-}
-
-
-test();
-
 app.get('/', async (req, res) => {
   const response = await UserModel.create({
     name: 'Ninad',
